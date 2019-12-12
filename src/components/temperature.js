b82cf60dcd84cab038982db115719354733cadef
Mathai Puthiakunnel <mathaiputhiakunnel@Mathais-MacBook-Air.local>
2019-12-12 12:48:19 -0500

    firebase initialization

--- src/components/temperature.js
+++ src/components/temperature.js
@@ -1,22 +1,12 @@
 import "../App.css";
 import React from "react";
-import firebase from "firebase";
 import Typography from "@material-ui/core/Typography";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import Divider from "@material-ui/core/Divider";
-import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import Select from "@material-ui/core/Select";
 import Slider from "@material-ui/core/Slider";
 import { makeStyles } from "@material-ui/core/styles";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
 
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import DoneAllIcon from "@material-ui/icons/DoneAll";
